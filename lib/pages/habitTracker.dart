0dc42f24616692c90cf2cda136570d1bd1334d75
zahar01 <ztimofeev0401@gmail.com>
2022-08-06 13:27:40 +0400

    Button for adding habitTiles and deleting button

--- lib/pages/habitTracker.dart
+++ lib/pages/habitTracker.dart
@@ -44,43 +44,93 @@ class _habitTrackerState extends State<habitTracker> {
     }
   }
 
+  void deleteHabit(int index) {
+    setState(() {
+      habitsList.remove(habitsList[index]);
+    });
+  }
+
   void settingsOpened(int index) {
     showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
             title: Text("Settings for ${habitsList[index][0]}"),
+            content: TextButton(
+              child: Text("DELETE"),
+              onPressed: () {
+                deleteHabit(index);
+              },
+            ),
           );
         });
+  }
+
+  // void addHabit() {
+  //   showDialog(
+  //       context: context,
+  //       builder: (context) {
+  //         return Column(
+  //           children: const [
+  //             TextField(
+  //               decoration: InputDecoration(
+  //                 hintText: "abooba",
+  //                 border: OutlineInputBorder(),
+  //               ),
+  //             ),
+  //             // SizedBox(height: 20),
+  //             // TimePickerDialog(initialTime: TimeOfDay(hour: 0, minute: 0)),
+  //           ],
+  //         );
+  //       });
+  // }
+
+  void addHabit() {
+    setState(() {
+      habitsList.add(["Read", false, 0, 1]);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.cyan[50],
-        appBar: AppBar(
-          backgroundColor: Colors.cyan[700],
-          title: const Text(
-            "Focus!",
-            style: TextStyle(color: Colors.white),
-          ),
-          //centerTitle: false,
+      backgroundColor: Colors.cyan[50],
+      appBar: AppBar(
+        backgroundColor: Colors.cyan[700],
+        title: const Text(
+          "Focus!",
+          style: TextStyle(color: Colors.white),
         ),
-        body: ListView.builder(
-            itemCount: habitsList.length,
-            itemBuilder: (context, index) {
-              return HabitTile(
-                habitName: habitsList[index][0],
-                onTap: () {
-                  habitStarted(index);
-                },
-                settingsTap: () {
-                  settingsOpened(index);
-                },
-                timeSpent: habitsList[index][2],
-                habitTime: habitsList[index][3],
-                habitStarted: habitsList[index][1],
-              );
-            }));
+        //centerTitle: false,
+      ),
+      body: ListView.builder(
+          itemCount: habitsList.length,
+          itemBuilder: (context, index) {
+            return HabitTile(
+              habitName: habitsList[index][0],
+              onTap: () {
+                habitStarted(index);
+              },
+              settingsTap: () {
+                settingsOpened(index);
+              },
+              timeSpent: habitsList[index][2],
+              habitTime: habitsList[index][3],
+              habitStarted: habitsList[index][1],
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addHabit();
+          print("added $habitsList");
+        },
+        backgroundColor: Colors.cyan[700],
+        child: const Icon(
+          Icons.add,
+          size: 40,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
